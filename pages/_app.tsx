d97f234695635a08e1679e8eb1aaf15cc62135ce
Paweł Kromołowski <cater981pl@gmail.com>
2022-09-26 09:25:21 +0200

    scss change all imports to use @use instead of @import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.scss';
+import '../styles/global.scss';
 import type { AppProps } from 'next/app';
 import { AuthContextProvider } from '../store/AuthContext';
 import { useRouter } from 'next/router';
